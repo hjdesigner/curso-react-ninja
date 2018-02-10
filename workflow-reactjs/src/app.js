fa9bc3a17e203e31e0901613f399b6e4fb57882b
Henrique Melanda <hjdesigner23@gmail.com>
2018-02-10 21:40:43 -0200

    Workflow ReactJS

---
+++ workflow-reactjs/src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+const createReactClass = require('create-react-class')
+
+const App = createReactClass({
+  render: function () {
+    return <h1>Titulo</h1>
+  }  
+})
+
+export default App
