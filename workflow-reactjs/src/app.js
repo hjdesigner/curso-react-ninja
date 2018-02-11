fc51305cd3f4ec7905e1ce0753c75721bcee3603
Henrique Melanda <hjdesigner23@gmail.com>
2018-02-11 12:19:38 -0200

    Modularizando o CSS

--- workflow-reactjs/src/app.js
+++ workflow-reactjs/src/app.js
@@ -1,11 +1,20 @@
 'use strict'
 
 import React from 'react'
+import Button from './components/search/search.js'
+
 const createReactClass = require('create-react-class')
 
 const App = createReactClass({
   render: function () {
-    return <h1>Titulo</h1>
+    return (
+      <div className='container'>
+        <Button>
+          <span>Texto</span>
+          outro texto
+        </Button>
+      </div>
+    )
   }
 })
 
